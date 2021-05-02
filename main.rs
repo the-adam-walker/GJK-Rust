1c71516187d9743487c433e6bdbaedfb25cb7e49
danieljpietz <danieljpietz@gmail.com>
2021-05-01 17:24:07 -0700

    Added furthestPoint function

--- main.rs
+++ main.rs
@@ -7,12 +7,6 @@ struct Shape {
 
 impl Shape {
 
-    fn init(&mut self, points: Vec<Vector3<f32>>) {
-        for point in points {
-            self.vecs.push(point);
-        }
-    }
-
     fn centroid(&mut self) -> Vector3<f32> {
         let mut center = self.vecs[0];
         for i in 1..self.vecs.len() {
@@ -20,11 +14,30 @@ impl Shape {
         }
         return center;
     }
+
+    fn init(&mut self, points: Vec<Vector3<f32>>) {
+        for point in points {
+            self.vecs.push(point);
+        }
+    }
+
+    fn furthestPoint(&mut self, d: Vector3<f32>) -> Vector3<f32> {
+        let mut dotMax = 0.0;
+        let mut currentVector = Vector3::<f32>::new(0.0,0.0,0.0);
+        for i in 0..self.vecs.len() {
+            let mut vec = self.vecs[i];
+            let mut dotCurrent = (vec - self.centroid()).dot(&d);
+            if (dotCurrent > dotMax) {
+                dotCurrent = dotMax;
+                currentVector = vec;
+            }
+        }
+        return currentVector;
+    }
 }
 
 
 
 
 fn main() {
-
 }
