fcb768dff3a9883e31718f79cf8916faeea5bbf0
danieljpietz <danieljpietz@gmail.com>
2021-05-01 17:09:58 -0700

    Added nalgebra library

--- main.rs
+++ main.rs
@@ -1,6 +1,30 @@
+use nalgebra::Vector3;
+
+
+struct Shape {
+    vecs: Vec<Vector3<f32>>,
+}
+
+impl Shape {
+
+    fn init(&mut self, points: Vec<Vector3<f32>>) {
+        for point in points {
+            self.vecs.push(point);
+        }
+    }
+
+    fn centroid(&mut self) -> Vector3<f32> {
+        let mut center = self.vecs[0];
+        for i in 1..self.vecs.len() {
+            center += self.vecs[i];
+        }
+        return center;
+    }
+}
+
+
+
+
 fn main() {
 
-
-
-    
 }
