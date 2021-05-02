afcf6ae6fefd41b0dbce0b0f58b96dd6d4ee0e73
Daniel <danieljpietz@gmail.com>
2021-05-02 10:02:04 -0700

    Refactored Code. Bug Fixes

--- main.rs
+++ main.rs
@@ -1,131 +1,119 @@
 use nalgebra::Vector3;
 
-
 struct Shape {
-    vecs: Vec<Vector3<f32>>,
+    vecs: Vec<Vector3<f64>>,
 }
 
 impl Shape {
 
-    fn centroid(&mut self) -> Vector3<f32> {
-        let mut center = self.vecs[0];
-        for i in 1..self.vecs.len() {
-            center += self.vecs[i];
-        }
-        return center;
-    }
+    fn centroid(&self) -> Vector3<f64> {
+        let mut center = Vector3::<f64>::new(0.0 as f64, 0.0 as f64, 0.0 as f64);
 
-    fn init(&mut self, points: Vec<Vector3<f32>>) {
-        for point in points {
-            self.vecs.push(point);
+        for vec in &self.vecs {
+            center += vec;
         }
+        return center/(self.vecs.len() as f64);
     }
-
-    fn furthestPoint(&mut self, d: &mut Vector3<f32>) -> Vector3<f32> {
-        let mut dotMax = 0.0;
-        let mut currentVector = Vector3::<f32>::new(0.0,0.0,0.0);
-        for i in 0..self.vecs.len() {
-            let mut vec = self.vecs[i];
-            let mut dotCurrent = (vec - self.centroid()).dot(&d);
-            if dotCurrent > dotMax {
-                dotMax = dotCurrent;
-                currentVector = vec;
+        fn furthest_point(&self, d: Vector3<f64>) -> Vector3<f64> {
+            let mut dot_max = 0.0;
+            let mut current_vector = self.vecs[0];
+           for vec in &self.vecs {
+                let dot_current = (vec - self.centroid()).dot(&d);
+                if dot_current > dot_max {
+                    dot_max = dot_current;
+                    current_vector = *vec;
+                }
             }
+            return current_vector;
         }
-        return currentVector;
-    }
+        
 }
 
-fn support(s1: &mut Shape, s2: &mut Shape, d: &mut Vector3<f32>) -> Vector3<f32> {
-    let mut reverse_d= *d;
-    reverse_d.x = -reverse_d.x;
-    reverse_d.y = -reverse_d.y;
-    reverse_d.z = -reverse_d.z;
-    return s1.furthestPoint(d) - s2.furthestPoint(&mut reverse_d);
+fn support(s1: &Shape, s2: &Shape, d: Vector3<f64>) -> Vector3<f64> {
+    return s1.furthest_point(d) - s2.furthest_point(-d);
 }
 
-
-fn tripProd(v1: Vector3<f32>,v2: Vector3<f32>, v3: Vector3<f32>) -> Vector3<f32> {
+fn trip_prod(v1: Vector3<f64>,v2: Vector3<f64>, v3: Vector3<f64>) -> Vector3<f64> {
     return (v1.cross(&v2)).cross(&v3);
 }
 
-
-fn lineCase(simplex: &mut Vec<Vector3<f32>>, d: &mut Vector3<f32>) -> bool {
-    let mut B = simplex[0];
-    let mut A = simplex[1];
-    let mut AB = B - A;
-    let mut AO = Vector3::<f32>::new(0.0, 0.0, 0.0) - A;
-    let mut ABPerp = tripProd(AB, AO, AB);
-    *d = ABPerp;
+fn line_case(simplex: &Vec<Vector3<f64>>, d: &mut Vector3<f64>) -> bool {
+    let b = simplex[0];
+    let a = simplex[1];
+    let ab = b - a;
+    let ao = -a;
+    let abperp = trip_prod(ab, ao, ab);
+    *d = abperp;
     return false;
 }
 
-fn triangleCase(simplex: &mut Vec<Vector3<f32>>, d: &mut Vector3<f32>) -> bool {
+fn triangle_case(simplex: &mut Vec<Vector3<f64>>, d: &mut Vector3<f64>) -> bool {
+    let c = simplex[simplex.len() - 3];
+    let b = simplex[simplex.len() - 2];
+    let a = simplex[simplex.len() - 1];
+    let ab = b - a;
+    let ac = c - a;
+    let ao = -a;
+    let abperp = trip_prod(ac, ab, ab);
+    let acperp = trip_prod(ab, ac, ac);
 
-    let mut C = simplex[simplex.len() - 3];
-    let mut B = simplex[simplex.len() - 2];
-    let mut A = simplex[simplex.len() - 1];
-
-    let mut AB = B - A;
-    let mut AC = C - A;
-    let mut AO = -A;
-
-    let mut ABPerp = tripProd(AC, AB, AB);
-    let mut ACPerp = tripProd(AB, AC, AC);
-
-    if ABPerp.dot(&AO) > 0.0 {
+    if abperp.dot(&ao) > 0.0 {
         simplex.remove(simplex.len() - 3);
-        *d = ABPerp;
+        *d = abperp;
         return false;
     }
-    else if ACPerp.dot(&AO) > 0.0 {
+    else if acperp.dot(&ao) > 0.0 {
         simplex.remove(simplex.len() - 2);
-        *d = ACPerp;
+        *d = acperp;
         return false;
-    }
-
-    return true;
-
-}
-
-fn handleSimplex(simplex: &mut Vec<Vector3<f32>>, d: &mut Vector3<f32>) -> bool {
-    if simplex.len() == 2 {
-        return lineCase(simplex, d);
     }
     else {
-        return triangleCase(simplex, d);
+        return true;
     }
 }
 
+fn handle_simplex(simplex: &mut Vec<Vector3<f64>>, d: &mut Vector3<f64>) -> bool {
+    return if simplex.len() == 2 {
+        line_case(simplex, d)
+    } else {
+        triangle_case(simplex, d)
+    }
+}
 
-fn GJK(s1: &mut Shape, s2: &mut Shape) -> bool {
-
-    //Vector3<T> d = (s2.centroid() - s1.centroid()).normalize();
+fn gjk(s1: &Shape, s2: &Shape) -> bool {
     let mut d = (s2.centroid() - s1.centroid()).normalize();
+    let mut simplex: Vec<Vector3<f64>> = vec![support(s1, s2, d)];
+    d = -simplex[0];
 
-    let mut simplex: Vec<Vector3<f32>> = vec![support(s1, s2, &mut d)];
-
-
-
-    d = Vector3::new(0.0,0.0,0.0) - simplex[0];
-
-    while true {
-        let mut A = support(s1, s2, &mut d);
-        if A.dot(&d) < 0.0 {
+    loop {
+        let a = support(s1,s2, d);
+        if a.dot(&d) < 0.0 {
             return false;
         }
         else {
-            simplex.push(A);
-            if handleSimplex(&mut simplex, &mut d) {
+            simplex.push(a);
+            if handle_simplex(&mut simplex, &mut d) {
                 return true;
             }
         }
     }
-
-    return true;
 }
 
 fn main() {
 
+
+    let s1 = Shape {
+        vecs: vec![Vector3::<f64>::new(0.0,0.0,0.0),
+                   Vector3::<f64>::new(0.0,1.0,0.0),
+                   Vector3::<f64>::new(1.0,0.0,0.0)],
+    };
+
+    let s2 = Shape {
+        vecs: vec![Vector3::<f64>::new(0.0 + 1.01,0.0,0.0),
+                   Vector3::<f64>::new(0.0 + 1.01,1.0,0.0),
+                   Vector3::<f64>::new(1.0 + 1.01,0.0,0.0)],
+    };
+
+    println!("{}", gjk(&s1, &s2));
 
 }
