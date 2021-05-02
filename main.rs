b03f6645a2bd75e838c17cfd6d506712fad2904a
Adam Walker <adamwalker2021@gmail.com>
2021-05-01 18:15:59 -0700

    added support and lineCase

--- main.rs
+++ main.rs
@@ -27,7 +27,7 @@ impl Shape {
         for i in 0..self.vecs.len() {
             let mut vec = self.vecs[i];
             let mut dotCurrent = (vec - self.centroid()).dot(&d);
-            if (dotCurrent > dotMax) {
+            if dotCurrent > dotMax {
                 dotCurrent = dotMax;
                 currentVector = vec;
             }
@@ -37,11 +37,25 @@ impl Shape {
 }
 
 fn support(s1: &mut Shape, s2: &mut Shape, d: &mut Vector3<f32>) -> Vector3<f32> {
-    return s1.furthestPoint(d) - s2.furthestPoint(-d);
+    let mut reverse_d= *d;
+    reverse_d.x = -reverse_d.x;
+    reverse_d.y = -reverse_d.y;
+    reverse_d.z = -reverse_d.z;
+    return s1.furthestPoint(d) - s2.furthestPoint(&mut reverse_d);
 }
 
 fn tripProd(v1: Vector3<f32>,v2: Vector3<f32>, v3: Vector3<f32>) -> Vector3<f32> {
     return (v1.cross(&v2)).cross(&v3);
+}
+
+fn lineCase(simplex: &mut Vec<Vector3<f32>>, d: &mut Vector3<f32>) -> bool{
+    let mut B = simplex[0];
+    let mut A = simplex[1];
+    let mut AB = B - A;
+    let mut AO = Vector3::<f32>::new(0.0,0.0,0.0) - A;
+    let mut ABPerp = tripProd(AB, AO, AB);
+    *d = ABPerp;
+    return false;
 }
 
 fn main() {
